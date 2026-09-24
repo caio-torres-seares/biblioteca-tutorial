96163bec648652b286eaef310e03f48777cd09c6
Caio <caiot.seares@gmail.com>
2026-09-24 17:08:48 -0300

    fix: corrigindo tabela que não era iniciada

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { buildUseCases } from "./composition";
 import { createAcervoTables } from "./modules/acervo";
+import { createAvaliacaoTables } from "./modules/acervo/infrastructure/schema";
 import { createAutoriaTables } from "./modules/autoria";
 import { createServer } from "./server";
 
 createAutoriaTables();
 createAcervoTables();
+createAvaliacaoTables();
 
 const server = createServer(buildUseCases());
 
